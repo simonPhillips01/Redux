7c8e600b20126e50af62a927ed27fdf551dcbbf4
Simon Phillips <simon.phillips01@icloud.com>
2018-09-15 16:19:06 +0100

    Added youtube library and added a search component

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyDLWVakQ7DHwfrRZNZR9eeWC7QCmRSeqbE';
 
 //Create a new component. This component should produce some HTML
 const App = function() {
-  return <div>Hi!</div>
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 //Take this components generated HTML and put it on the page (in the DOM)
