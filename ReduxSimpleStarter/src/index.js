7c03c8777fd20b36d4d544df8ad8907f31e32cc1
Simon Phillips <simon.phillips01@icloud.com>
2018-09-19 23:06:12 +0100

    Refactoring functional components to class components and hiding api keys

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import API_K from '.././credentials.js';
 
-const API_KEY = 'AIzaSyDLWVakQ7DHwfrRZNZR9eeWC7QCmRSeqbE';
+const API_KEY = API_K;
 
-//Create a new component. This component should produce some HTML
-const App = function() {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        </div>
+      );
+    }
+  }
 
 //Take this components generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
